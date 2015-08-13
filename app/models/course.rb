79c95fe8dad815cc5fe7080b11f211a38e151317
Matheus Lucena <matheuslucena@gmail.com>
2015-08-13 10:40:58 -0300

    Added validates to required fields

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,6 @@
 class Course < ActiveRecord::Base
+  validates_presence_of :name, :description
+
   has_many :classrooms
   has_many :students, through: :classrooms
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,5 @@
 class Student < ActiveRecord::Base
-    validates_presence_of :name, :register_number
+  validates_presence_of :name, :register_number
 
   has_many :classrooms
   has_many :courses, through: :classrooms
